1ce0b85531a9c752358ed5e2d38e7bc941ef3221
Silas <saosilas@hotmail.com>
2022-10-27 13:54:01 -0300

    mais campos de telefone

---
+++ Silas.Web/wwwroot/NovoSite/src/js/pages/Cliente/AdcCampo.js
@@ -0,0 +1,6 @@
+﻿var controleCampo = 1;
+function adcCampo() {
+    controleCampo++;
+    document.getElementById('telefone').insertAdjacentHTML('afterend','<div class="form-group row" id="telefone"><label for="example-password-input" class="col-2 col-form-label">Telefone</label><div class="col-10"><output class="form-control" type="number" name="telefone"></output></div>');
+}
+
